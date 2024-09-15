95eb14084fa67c077065cebc413dce30b88682d8
Gourav Chaturvedi <chaturvedigourav47@gmail.com>
2024-09-15 18:07:12 +0530

    Home Page Design Completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/signup" element={<SignUpPage />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage/>} />
         </Routes>
       </div>
       <Toaster />
