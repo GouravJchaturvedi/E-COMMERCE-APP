34b2f302b31336aea52514192021037a76c31a82
Gourav Chaturvedi <chaturvedigourav47@gmail.com>
2024-09-15 16:06:23 +0530

    SignUp UI Design completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,24 @@
-import { Routes , Route } from "react-router-dom"
-import HomePage from "./Pages/HomePage"
-import SignUpPage from "./Pages/SignUpPage"
-import LoginPage from "./Pages/LoginPage"
-import Navbar from "./components/Navbar"
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
+import SignUpPage from "./Pages/SignUpPage";
+import LoginPage from "./Pages/LoginPage";
+import Navbar from "./components/Navbar";
+import { Toaster } from "react-hot-toast";
 
 function App() {
-  
   return (
-    <div className="min-h-screen bg-gray-200 text-white relative overflow-hidden">
-    <Navbar/>
-    <Routes>
-      <Route path='/' element={<HomePage/>}></Route>
-      <Route path='/signup' element={<SignUpPage/>}></Route>
-      <Route path='/login' element={<LoginPage/>}></Route>
-    </Routes>
+    <div>
+      <div className="min-h-screen bg-gray-100 text-white relative overflow-hidden">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </div>
+      <Toaster />
     </div>
-    
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/Pages/LoginPage.jsx
+++ frontend/src/Pages/LoginPage.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { useState } from 'react'
+import {motion} from "framer-motion"
+import { Link } from 'react-router-dom'
 
 function LoginPage() {
+  const [email , setEmail] = useState("");
+  const [password , setPassword] = useState("");
+  const loading = true;
+
+  const handleSumbit = (e) =>{
+    e.preventDefault();
+    console.log(email , password)
+  }
   return (
     <div>
-      Login PAhe hai
+
     </div>
   )
 }
